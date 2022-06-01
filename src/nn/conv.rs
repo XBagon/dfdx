9f4e9923e4bf596eb35493ecf41f5e255c17d72f
Corey Lowman <clowman1993@gmail.com>
2022-06-01 13:10:40 -0400

    #1 adding col2im

--- src/nn/conv.rs
+++ src/nn/conv.rs
@@ -52,9 +52,11 @@ where
 
     fn forward(&self, input: Tensor3D<IC, IH, IW, H>) -> Self::Output {
         let (input, tape) = input.split_tape();
-        let col = im2col::<IC, IH, IW, { out_size(IH, P, K, S) }, { out_size(IW, P, K, S) }, K, S, P>(
-            input.data(),
-        );
+        // let mut col = Cpu::zeros();
+        // im2col::<IC, IH, IW, { out_size(IH, P, K, S) }, { out_size(IW, P, K, S) }, K, S, P>(
+        //     input.data(),
+        //     col.as_mut(),
+        // );
         todo!();
     }
 }
@@ -70,31 +72,62 @@ fn im2col<
     const PADDING: usize,
 >(
     im: &[[[f32; IN_WIDTH]; IN_HEIGHT]; IN_CHANNELS],
-) -> Box<[[f32; OUT_HEIGHT * OUT_WIDTH]; IN_CHANNELS * KERNEL_SIZE * KERNEL_SIZE]>
-where
+    col: &mut [[f32; OUT_HEIGHT * OUT_WIDTH]; IN_CHANNELS * KERNEL_SIZE * KERNEL_SIZE],
+) where
     [(); IN_CHANNELS * KERNEL_SIZE * KERNEL_SIZE]: Sized,
     [(); OUT_HEIGHT * OUT_WIDTH]: Sized,
 {
-    let mut output: Box<[[f32; OUT_HEIGHT * OUT_WIDTH]; IN_CHANNELS * KERNEL_SIZE * KERNEL_SIZE]> =
-        Cpu::zeros();
-    for out_0 in 0..IN_CHANNELS * KERNEL_SIZE * KERNEL_SIZE {
-        let in_channel = out_0 / (KERNEL_SIZE * KERNEL_SIZE);
-        let in_kernel_ind = out_0 % (KERNEL_SIZE * KERNEL_SIZE);
+    for col_0 in 0..IN_CHANNELS * KERNEL_SIZE * KERNEL_SIZE {
+        let im_channel = col_0 / (KERNEL_SIZE * KERNEL_SIZE);
+        let in_kernel_ind = col_0 % (KERNEL_SIZE * KERNEL_SIZE);
         let h_offset = in_kernel_ind / KERNEL_SIZE;
         let w_offset = in_kernel_ind % KERNEL_SIZE;
-        for out_1 in 0..OUT_HEIGHT * OUT_WIDTH {
-            debug_assert_eq!(output[out_0][out_1], 0.0);
-            let out_height = out_1 / OUT_WIDTH;
-            let out_width = out_1 % OUT_WIDTH;
-            let in_height = (out_height * STRIDE + h_offset).checked_sub(PADDING);
-            let in_width = (out_width * STRIDE + w_offset).checked_sub(PADDING);
-            output[out_0][out_1] = match (in_height, in_width) {
-                (Some(h), Some(w)) if h < IN_HEIGHT && w < IN_WIDTH => im[in_channel][h][w],
+        for col_1 in 0..OUT_HEIGHT * OUT_WIDTH {
+            debug_assert_eq!(col[col_0][col_1], 0.0);
+            let im_height = ((col_1 / OUT_WIDTH) * STRIDE + h_offset).checked_sub(PADDING);
+            let im_width = ((col_1 % OUT_WIDTH) * STRIDE + w_offset).checked_sub(PADDING);
+            col[col_0][col_1] = match (im_height, im_width) {
+                (Some(h), Some(w)) if h < IN_HEIGHT && w < IN_WIDTH => im[im_channel][h][w],
                 _ => 0.0,
             };
         }
     }
-    output
+}
+
+fn col2im<
+    const IN_CHANNELS: usize,
+    const IN_HEIGHT: usize,
+    const IN_WIDTH: usize,
+    const OUT_HEIGHT: usize,
+    const OUT_WIDTH: usize,
+    const KERNEL_SIZE: usize,
+    const STRIDE: usize,
+    const PADDING: usize,
+>(
+    col: &[[f32; OUT_HEIGHT * OUT_WIDTH]; IN_CHANNELS * KERNEL_SIZE * KERNEL_SIZE],
+    im: &mut [[[f32; IN_WIDTH]; IN_HEIGHT]; IN_CHANNELS],
+) where
+    [(); IN_CHANNELS * KERNEL_SIZE * KERNEL_SIZE]: Sized,
+    [(); OUT_HEIGHT * OUT_WIDTH]: Sized,
+{
+    for col_0 in 0..IN_CHANNELS * KERNEL_SIZE * KERNEL_SIZE {
+        let im_channel = col_0 / (KERNEL_SIZE * KERNEL_SIZE);
+        let in_kernel_ind = col_0 % (KERNEL_SIZE * KERNEL_SIZE);
+        let h_offset = in_kernel_ind / KERNEL_SIZE;
+        let w_offset = in_kernel_ind % KERNEL_SIZE;
+        for col_1 in 0..OUT_HEIGHT * OUT_WIDTH {
+            let im_height = ((col_1 / OUT_WIDTH) * STRIDE + h_offset).checked_sub(PADDING);
+            let im_width = ((col_1 % OUT_WIDTH) * STRIDE + w_offset).checked_sub(PADDING);
+            match (im_height, im_width) {
+                (Some(h), Some(w)) if h < IN_HEIGHT && w < IN_WIDTH => {
+                    im[im_channel][h][w] = col[col_0][col_1]
+                }
+                _ => {
+                    debug_assert_eq!(col[col_0][col_1], 0.0);
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -109,10 +142,11 @@ mod tests {
             [[-1.0, -2.0, -3.0, -4.0], [-6.0, -7.0, -8.0, -9.0]],
             [[0.0, 0.1, 0.2, 0.3], [0.5, 0.6, 0.7, 0.8]],
         ];
+        let mut out = [[0.0f32; 3]; 12];
 
-        let col: Box<[[f32; 3]; 12]> = im2col::<3, 2, 4, 1, 3, 2, 1, 0>(&image);
+        im2col::<3, 2, 4, 1, 3, 2, 1, 0>(&image, &mut out);
         assert_eq!(
-            col.as_ref(),
+            &out,
             &[
                 [1.0, 2.0, 3.0],
                 [2.0, 3.0, 4.0],
@@ -128,5 +162,22 @@ mod tests {
                 [0.6, 0.7, 0.8]
             ]
         );
+    }
+
+    #[test]
+    fn test_col2im_reverses_im2col() {
+        let image = [
+            [[1.0, 2.0, 3.0, 4.0], [6.0, 7.0, 8.0, 9.0]],
+            [[-1.0, -2.0, -3.0, -4.0], [-6.0, -7.0, -8.0, -9.0]],
+            [[0.0, 0.1, 0.2, 0.3], [0.5, 0.6, 0.7, 0.8]],
+        ];
+
+        let mut col = [[0.0f32; 3]; 12];
+        im2col::<3, 2, 4, 1, 3, 2, 1, 0>(&image, &mut col);
+
+        let mut image2 = [[[0.0f32; 4]; 2]; 3];
+        col2im::<3, 2, 4, 1, 3, 2, 1, 0>(&col, &mut image2);
+
+        assert_eq!(image, image2);
     }
 }
